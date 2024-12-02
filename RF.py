82182093f315ff00478b5f058db3c2bf24e70cc5
DDDCR <156021143+DDDCR@users.noreply.github.com>
2024-12-02 22:19:05 +0200

    Add files via upload
    uptade the fine tuned RF model

--- RF.py
+++ RF.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.ensemble import RandomForestRegressor
+from scipy import stats
 
 # Load the training and test datasets
 train_df = pd.read_csv('train.csv')
@@ -22,17 +22,21 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_val, y_train, y_val = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42)
 
-rf = RandomForestRegressor(random_state=42)
-parameters = {'n_estimators': [100, 200],
-            'max_depth': [None, 10, 20],
-            'min_samples_split': [2, 5]}
-rf_reg = GridSearchCV(rf, parameters, cv=5)
-rf_reg.fit(X_train, y_train)
+model = RandomForestRegressor(
+    n_estimators=300,
+    max_depth=10,
+    max_features='sqrt',
+    min_samples_leaf=4,
+    min_samples_split=2,
+    random_state=42
+)
+
+model.fit(X_scaled, y)
 
 X_test = test_df[features]
 X_test_scaled = scaler.transform(X_test)
 
-test_predictions = rf_reg.predict(X_test_scaled)
+test_predictions = model.predict(X_test_scaled)
 
 submission = pd.DataFrame({
     'ID': test_df['ID'],
@@ -43,4 +47,4 @@ submission = pd.DataFrame({
 submission['TARGET'] = submission['TARGET'].round(9)
 
 # Save to CSV
-submission.to_csv('submission.csv', index=False)
+submission.to_csv('submission2.csv', index=False)
